d7203b824d6bf9b757114232761213ec21004edf
Chris Wand <chris.wand@gmail.com>
2014-12-17 13:59:01 -0500

    .

--- spec/features/user_starts_a_new_game_spec.rb
+++ spec/features/user_starts_a_new_game_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
-require 'pry'
 
 feature "User starts a new game" do
   scenario "User sees a new board of cards" do
@@ -10,7 +9,6 @@ feature "User starts a new game" do
     (0..11).each do |index|
       expect(page).to have_css("div.card##{index}")
     end
-    binding.pry
 
   end
 end
